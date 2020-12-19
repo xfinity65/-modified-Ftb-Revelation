4d4801fb442192c72bae7064c904489ed74afbf3
xfinity65 <74889458+xfinity65@users.noreply.github.com>
2020-12-19 13:27:50 -0800

    old files

--- server/start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "Do you agree to the Mojang EULA available at https://account.mojang.com/documents/minecraft_eula ?"
-set /p EULA=[y/n]
-IF /I "%EULA%" NEQ "y" GOTO END
-echo eula=true>eula.txt
-:END
-java.exe -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.12.2-14.23.5.2846-universal.jar nogui
--- server/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Do you agree to the Mojang EULA available at https://account.mojang.com/documents/minecraft_eula ?"
-EULA=`read  -n 1 -p "[y/n] "`
-if [ "$EULA" = "y" ]; then
-    echo "eula=true" > eula.txt
-fi
-java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Xmx6144M -Xms4096M -jar forge-1.12.2-14.23.5.2846-universal.jar nogui
